47650191ff5e4ff1e9d2c1eac77bf6424d5ca541
Vinicius Costa <vcdl1997@gmail.com>
2021-08-29 20:22:37 -0300

    Primeiro projeto desenvolvido em React. Aplicação simples apenas abordando o conceito de componentes.

---
+++ src/componentes/HomePage/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from '../Card';
+import './style.css';
+
+function HomePage(){
+    return(
+        <main class="home-page">
+            <Card/>
+            <Card/>
+            <Card/>
+        </main>
+    );
+}
+
+export default HomePage;
